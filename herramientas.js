6f335acb3b2619b85dc632f713142e08ffb08a76
Aguszar <agustinzarzur4@gmail.com>
2025-08-26 06:19:14 -0300

    sierra

--- herramientas.js
+++ herramientas.js
@@ -4,5 +4,5 @@ herramientas = [
     ["Compresor", "$80.000", "Marca Robust, tiene una perdida de aire", "herramientas/IMG_20250807_080620082_AE.jpg"],
     ["Cepilladora de madera", "$60.000","Marca Gladiator. En excelente estado", "herramientas/IMG_20250805_144558117_HDR_AE.jpg"],
     ["Mochila Fumigadora","$25.000", "Está manchada, pero el funcionamiento anda bien", "herramientas/IMG_20250807_081244513_AE.jpg"],
-    
+    ["Sierra Circular", "$90.000", "Diametro??, marca Skil", "herramientas/IMG_20250805_145251075_AE.jpg"]
 ]
