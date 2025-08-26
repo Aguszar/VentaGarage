4850c9fb3ce8895166fa817ef145a5f914d656ba
Aguszar <agustinzarzur4@gmail.com>
2025-08-26 10:34:20 -0300

    parte cocina

---
+++ cocina.js
@@ -0,0 +1,18 @@
+cocina = [
+    [false, "Bowl Vidrio", "$5000", "24 cm de diametro y 6 cm de alto", "cocina/IMG_20250826_091557636_AE.jpg"],
+    [false, "Bandeja", "$5000", "bandeja de chapa. 26x15 cm", "cocina/IMG_20250826_091616379_AE.jpg"],
+    [false, "Platos Ceramica", "$3000", "Platos hondos blancos de ceramica 22.5 cm diametro", "cocina/IMG_20250826_091700315_AE.jpg"],
+    [false, "Platos Vidrio", "$3000", "23 cm de diametro", "cocina/IMG_20250826_091758311_AE.jpg"],
+    [false, "Triturador de verdura", "$6000", "En buen estado", "cocina/IMG_20250826_091832201_AE.jpg"],
+    [false, "Sartenes", "$8000", "", "cocina/IMG_20250826_093438024_AE.jpg"],
+    [false, "Pava", "$5000", "Pava antigua en buen estado", "cocina/IMG_20250826_093847477_AE.jpg"],
+    [false, "Bandejas horno", "$7000", "", "cocina/IMG_20250826_093933288_AE.jpg"],
+    [false, "Platos vidrio postre", "$2000", "", "cocina/IMG_20250826_094044725_AE.jpg"],
+    [false, "Bowl chicos", "$500", "", "cocina/IMG_20250826_094052342_AE.jpg"],
+    [false, "Rayador", "$1.000", "", "cocina/IMG_20250826_094309817_HDR_AE.jpg"],
+    [false, "Batidora", "$40.000", "Los 2 bowl incluido", "cocina/IMG_20250826_094350365_AE.jpg"],
+    [false, "", "", "", ""],
+    [false, "", "", "", ""],
+    [false, "", "", "", ""],
+    
+]
