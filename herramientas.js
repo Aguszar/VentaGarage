e0842735e731c99cb3a0601899939066ce886c26
Aguszar <agustinzarzur4@gmail.com>
2025-08-26 06:32:22 -0300

    solda

--- herramientas.js
+++ herramientas.js
@@ -1,5 +1,5 @@
 herramientas = [
-    ["Soldadora", "$70.000", "Marca Gamma, buen estado", "herramientas/solda.jpg"], 
+    ["Soldadora", "$70.000", "Marca Gamma, buen estado", "herramientas/IMG_20250802_112723035_AE.jpg"], 
     ["Tubos Crossman", "$80.000", "Muy buenas", "herramientas/tubos.jpg"],
     ["Compresor", "$80.000", "Marca Robust, tiene una perdida de aire", "herramientas/IMG_20250807_080620082_AE.jpg"],
     ["Cepilladora de madera", "$60.000","Marca Gladiator. En excelente estado", "herramientas/IMG_20250805_144558117_HDR_AE.jpg"],
