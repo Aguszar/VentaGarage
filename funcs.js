66ec4ce678c1cd9e0e08e5cfafacb46287bd94e6
Aguszar <agustinzarzur4@gmail.com>
2025-08-27 11:31:29 -0300

    actualizacion estado de venta

--- funcs.js
+++ funcs.js
@@ -8,18 +8,23 @@ function categoria(nombre, index) {
     productos.innerHTML = ""
     cat = categorias[index]
     for (let i = 0; i < cat.length; i++) {
-        if(cat[i][0]){// si fue vendido, tiene un cartel de vendido
+        if(cat[i][0]==1){// si fue vendido, tiene un cartel de vendido
             prod ="<div class='producto_vendido'>"
-        }else{
+        }if(cat[i][0]==3){// esta reservado
+            prod = "<div class='producto_reservado'>"
+        }if(cat[i][0]==0){
             prod = "<div class='producto'>"
         }
         
         prod += "<h3>"+cat[i][1]+"</h3>"
         prod +="<img src='imagenes/"+cat[i][4]+"' alt='fallo carga de imagen'>"
         prod +="<h4>Precio: "+cat[i][2]+"</h4>"
-        if(cat[i][0]){// si fue vendido, tiene un cartel de vendido
-            prod +="<p style='color:red; font-weight: bold;'>Vendido</p></div>"            
-        }else{
+
+        if(cat[i][0]==1){// si fue vendido, tiene un cartel de vendido
+            prod +="<p style='color:red; font-weight: bold;'>Vendido</p></div>"
+        }if(cat[i][0]==3){// esta reservado
+            prod +="<p style='color:yellow; font-weight: bold;'>Reservado</p></div>"
+        }if(cat[i][0]==0){
             prod +="<p>"+cat[i][3]+"</p></div>"
         }
         
--- muebles.js
+++ muebles.js
@@ -1,7 +1,7 @@
 muebles=[
-    [false, "Mesas de Luz", "$30.000", "", "muebles/IMG_20250826_120644498_AE.jpg"],
-    [false, "Colchones", "$20.000", "cada uno", "muebles/IMG_20250826_120758236_AE.jpg"],
-    [false, "Camas", "$20.000 cada uno", "tiene todos los tornillos", "muebles/IMG_20250826_120758236_AE.jpg"],
-    [false, "Armario Grande", "$70.000", "1.80 m de ancho, 2 metros de alto y 50 cm de profundo", "muebles/IMG_20250826_121113344_AE.jpg"],
+    [0, "Mesas de Luz", "$30.000", "", "muebles/IMG_20250826_120644498_AE.jpg"],
+    [0, "Colchones", "$20.000", "cada uno", "muebles/IMG_20250826_120758236_AE.jpg"],
+    [0, "Camas", "$20.000 cada uno", "tiene todos los tornillos", "muebles/IMG_20250826_120758236_AE.jpg"],
+    [0, "Armario Grande", "$70.000", "1.80 m de ancho, 2 metros de alto y 50 cm de profundo", "muebles/IMG_20250826_121113344_AE.jpg"],
     
 ]
