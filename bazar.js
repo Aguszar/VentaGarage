bb8ac1d57255c203f3221407a89847aaa122ca77
Aguszar <agustinzarzur4@gmail.com>
2025-08-28 16:46:30 -0300

    muchas cosas

--- bazar.js
+++ bazar.js
@@ -2,6 +2,20 @@ bazar=[
     [3,"Bicicleta", "$40.000", "Rodado 26", "bazar/IMG_20250802_112603828_AE.jpg"],
     [0,"Bidon Termico", "$10.000", "Bidon termico de 5 litros marca obba", "bazar/IMG_20250826_094443066_AE.jpg"],
     [3, "Ventilador", "$35.000", "", "bazar/IMG_20250826_121020710_AE.jpg"],
-    [0, "Carpa chica", "$20.000", "Carpa para 2 personas", "bazar/IMG_20250826_121211962_AE.jpg"],
+    [0, "Carpa", "$20.000", "Carpa para 2 personas", "bazar/IMG_20250828_110344438_HDR_AE.jpg"],
+    [0, "Carpa media sombra", "$5.000", "Le faltan unos caños", "bazar/IMG_20250828_115401282_AE.jpg"],
     [0, "Mancuerna+pesas", "$60.000", "2 mancuernas + 4 pesas de 1.25 kg + 6 pesas de 2.5 kg + 4 pesas de 5 kg", "bazar/IMG_20250826_135115461_AE.jpg"]
+    [0, "Televisor LED 24 PULGADAS", "$50.000", "", "bazar/IMG_20250826_091441940_HDR_AE.jpg"],
+    [0, "Maquina para hacer helado", "$15.000", "Heladora Antartida muy buen estado", "bazar/IMG_20250828_132541114_AE.jpg"],
+    [0, "Velador Antiguo", "$3.000", "", "bazar/IMG_20250828_132802893_HDR_AE.jpg"],
+    [0, "Velador con agarre", "$3.000", "", "bazar/IMG_20250828_141852651_HDR_AE.jpg"],
+    [0, "Bowl Decorativo Rojo", "$4.000", "", "bazar/IMG_20250828_132901557_HDR_AE.jpg"],
+    [0, "Hueveras de Madera", "$2.000 las 3", "", "bazar/IMG_20250828_132911680_AE.jpg"],
+    [0, "Espejo", "$3.000", "Espejito esppejito quien es la mas bonita ?", "bazar/IMG_20250828_132928966_AE.jpg"],
+    [0, "Parlantes USB", "$5.000", "", "bazar/IMG_20250828_133209966_AE.jpg"],
+    [0, "Joystick para televisor con JUEGOS ARCADE", "$10.000", "Tiene PACMAN, GALAGA, MAPPY, CARRERA DE AUTOS y Xevious", "bazar/IMG_20250828_135137210_AE.jpg"],    
+    [0, "Pistola silicona caliente", "$2.000", "en buen estado", "bazar/IMG_20250828_134545457_AE.jpg"],    
+    [0, "Plato para Picada", "$3.000", "", "bazar/IMG_20250828_134254850_AE.jpg"],    
+    [0, "Bnadejas hieleras", "$1.000 los 2 juntos", "", "bazar/IMG_20250828_132845979_AE.jpg"],    
+    
 ]
