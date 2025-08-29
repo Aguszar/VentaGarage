d21892e2bab96fe6d3c2449b4497554a053151d9
Aguszar <agustinzarzur4@gmail.com>
2025-08-29 06:57:00 -0300

    sdf

--- muebles.js
+++ muebles.js
@@ -1,10 +1,10 @@
 muebles=[
-    [0, "Mesas de Luz", "$10.000", "", "muebles/IMG_20250826_120644498_AE.jpg"],
+    [0, "Mesas de Luz", "$10.000 cada uno", "", "muebles/IMG_20250826_120644498_AE.jpg"],
     [0, "Colchones", "$20.000", "cada uno", "muebles/IMG_20250826_120758236_AE.jpg"],
     [0, "Camas", "$20.000 cada uno", "tiene todos los tornillos", "muebles/IMG_20250826_120758236_AE.jpg"],
     [0, "Armario Grande", "$70.000", "1.80 m de ancho, 2 metros de alto y 50 cm de profundo", "muebles/IMG_20250826_121113344_AE.jpg"],
     [0, "Alacena", "$90.000", "En muy lindo estado, 1 metro de ancho", "muebles/IMG_20250828_153234686_HDR_AE.jpg"],
-    [0, "Escritorio de madera", "$20.000", "Medio golpeado pero firme", "muebles/IMG_20250828_085458156_AE.jpg"]
+    [0, "Escritorio de madera", "$20.000", "Medio golpeado pero firme", "muebles/IMG_20250828_085458156_AE.jpg"],
     [0, "Mesa + 6 sillas", "$150.000", "", "muebles/IMG_20250829_053919820_AE.jpg"],
     [0, "Heladera con freezer", "$110.000", "Funciona excelente", "muebles/IMG_20250828_153329945_AE.jpg"],
 ]
